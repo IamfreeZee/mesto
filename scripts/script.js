190ecd2f0d8ede913b18f452ae06e4db0435d478
freeZeeee <freeze_o@mail.ru>
2023-03-30 16:14:05 +0300

    style: ренейминг элементов попапа редактирования профиля

--- scripts/script.js
+++ scripts/script.js
@@ -1,59 +1,63 @@
-// логика попапа
-// список переменных/const
+// список глобальных переменных
+// переменная попапа редактирования профиля
+const popupProfileEditElement = document.querySelector ('.popup-profile-edit');
 // переменные кнопок
-const popupElement = document.querySelector ('.popup');
-const popupOpenButtonElement = document.querySelector ('.profile__button-edit');
-const popupCloseButtonElement = popupElement.querySelector ('.popup__button-close');
-const popupSaveButtonElement = popupElement.querySelector ('.popup__button-save');
+const popupEditButtonElement = document.querySelector ('.profile__button-edit');
+const popupCloseButtonElement = popupProfileEditElement.querySelector ('.popup__button-close');
+const popupSaveButtonElement = popupProfileEditElement.querySelector ('.popup__button-save');
 
 // переменные инпутов в профиле
 const profileNameElement = document.querySelector ('.profile__user-name');
 const profileCaptionElement = document.querySelector ('.profile__user-caption');
 
 // переменные инпутов в форме попапа
-const formElement = popupElement.querySelector ('.popup__form');
+const formElement = popupProfileEditElement.querySelector ('.popup__form');
 const nameInputElement = formElement.querySelector ('.popup__input_user_name');
 const captionInputElement = formElement.querySelector ('.popup__input_user_caption');
 
-// список функций/function
+// список функций
 // функция открытия попапа
-function openPopup () {
-  popupElement.classList.add('popup_opened');
-  nameInputElement.value = profileNameElement.textContent;
-  captionInputElement.value = profileCaptionElement.textContent;
+function openPopup (element) {
+  element.classList.add('popup_opened');
 }
 
 // функция закрытия попапа
-function closePopup () {
-  popupElement.classList.remove('popup_opened');
+function closePopup (element) {
+  element.classList.remove('popup_opened');
 }
 
-// функция отправки формы
+// функция отправки формы редактирования профиля
 function handleFormSubmit (event) {
   event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   profileNameElement.textContent = nameInputElement.value;
   profileCaptionElement.textContent = captionInputElement.value;
-  closePopup ();
+  closePopup (popupProfileEditElement);
 }
 
-// обработчики/слушатели событий/eventListener
+// обработчики событий
 // обработчик события - открытие попапа по кнопке ручки
-popupOpenButtonElement.addEventListener ('click', openPopup);
+popupEditButtonElement.addEventListener ('click', function () {
+  nameInputElement.value = profileNameElement.textContent;
+  captionInputElement.value = profileCaptionElement.textContent;
+  openPopup(popupProfileEditElement);
+});
 
 // обработчик события - закрытие попапа по кнопке крестика
-popupCloseButtonElement.addEventListener ('click', closePopup);
+popupCloseButtonElement.addEventListener ('click', function () {
+  closePopup(popupProfileEditElement);
+});
 
 // обработчик события - закрытие попапа по клику за областью попапа
-popupElement.addEventListener('click', function (event) {
+popupProfileEditElement.addEventListener('click', function (event) {
   if (event.target === event.currentTarget) {
-    closePopup ();
+    closePopup (popupProfileEditElement);
   }
 });
 
-// обработчик события - отправка формы
+// обработчик события - отправка формы редактирования профиля
 formElement.addEventListener ('submit', handleFormSubmit);
 
-//////////////PR5///////////////
+////////////// PR5 ///////////////
 const log = console.log;
 // массив из объектов-карточек с фото
 const initialCards = [
